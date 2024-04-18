d1853fa823de5db86fdab313fb6de4dd9ed896d7
Parker Nilson <parker.todd.nilson@gmail.com>
2024-04-17 21:33:21 -0600

    Beautify the login page

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 var appTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+  colorScheme: const ColorScheme(
+    primary: Color(0xFF6497DD),
+    onPrimary: Colors.white,
+    secondary: Color(0xFF1F225A),
+    onSecondary: Colors.white,
+    error: Color(0xFFFF6815),
+    onError: Colors.white,
+    background: Colors.white,
+    onBackground: Color(0xFF2C2C2C),
+    surface: Colors.white,
+    onSurface: Color(0xFF2C2C2C),
+    brightness: Brightness.light
+  ),
   useMaterial3: true,
 );
